64953d6a8ca7f8ac44212b9cb60a42c414808ed8
oraman <oraman@naver.com>
2022-11-29 14:38:17 +0900

    first commit and push action

---
+++ park/src/park/Hello.java
@@ -0,0 +1,7 @@
+package park;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("hello1");
+	}
+}
---
+++ park/src/park/world01.java
@@ -0,0 +1,7 @@
+package park;
+
+public class world01 {
+	public static void main(String[] args) {
+		System.out.println("world01");
+	}
+}
